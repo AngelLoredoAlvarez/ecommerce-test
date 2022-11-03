ee9bdc7bdd0bfbdeeb6df31b96c8684a9f170cab
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-03 17:28:33 -0600

    Added the [id].tsx page that will allow the user to modify (or not) the selected product

---
+++ src/pages/productos/modificar/[id].tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+const EditProductPage: NextPage = () => {
+  const router = useRouter();
+  return (
+    <>
+      <h1>Modificar Producto</h1>
+    </>
+  );
+};
+
+export default EditProductPage;
