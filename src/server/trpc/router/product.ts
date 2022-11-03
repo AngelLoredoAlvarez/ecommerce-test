9000af3f266f177308491616cff72532ea40fa31
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-03 17:51:21 -0600

    Added the getProductToEdit prop that will execute a query to retrive all the data from a specific Product to populate the form in the UI

--- src/server/trpc/router/product.ts
+++ src/server/trpc/router/product.ts
@@ -1,7 +1,17 @@
+import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const productRouter = router({
   allProducts: publicProcedure.query(({ ctx }) =>
     ctx.prisma.product.findMany()
   ),
+  getProductToEdit: publicProcedure
+    .input(z.object({ productId: z.string() }))
+    .query(({ ctx, input }) =>
+      ctx.prisma.product.findUnique({
+        where: {
+          id: input.productId,
+        },
+      })
+    ),
 });
