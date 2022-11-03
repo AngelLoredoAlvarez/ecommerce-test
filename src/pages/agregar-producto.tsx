0f068247faf9ad9d38e1729f54806e159b59c26b
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-03 14:25:06 -0600

    Added the <ProductForm /> component just for testing

--- src/pages/agregar-producto.tsx
+++ src/pages/agregar-producto.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import { ProductForm } from "../components/ProductForm";
 
 const AddProductPage: NextPage = () => {
-  return <div>Agregar Producto Page</div>;
+  return (
+    <>
+      <div>Agregar Producto Page</div>
+      <ProductForm action="add" />
+    </>
+  );
 };
 
 export default AddProductPage;
