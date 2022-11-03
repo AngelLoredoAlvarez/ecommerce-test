74cdfd65c9634d66ed90e4b6721ffd484b58e8bd
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-03 09:54:31 -0600

    Added the <Layout /> component just for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from "next";
 import Link from "next/link";
 
 import { trpc } from "../utils/trpc";
+import { Layout } from "../components/Layout";
 
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
+      <Layout />
       <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
         <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
           Create <span className="text-purple-300">T3</span> App
