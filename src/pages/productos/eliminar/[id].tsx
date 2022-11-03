93414a8dd4a01cba39f422cebe01eff57d0196ef
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-03 17:27:49 -0600

    Added the [id].tsx page that will allow the user to delete (or not) the selected product

---
+++ src/pages/productos/eliminar/[id].tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+
+const DeleteProductPage: NextPage = () => {
+  const router = useRouter();
+  return (
+    <>
+      <h1>Eliminar Producto</h1>
+    </>
+  );
+};
+
+export default DeleteProductPage;
