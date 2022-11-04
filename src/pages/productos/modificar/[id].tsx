358dd5be77e04f9a63ca2564b564de9898e2159c
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-03 18:23:21 -0600

    Added the code to execute the getProductToEdit.useQuery() query to get all the information of the selected product, also, added the <ProductForm /> component, and spreaded the result of the query to get access to all the data inside this component

--- src/pages/productos/modificar/[id].tsx
+++ src/pages/productos/modificar/[id].tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 
+import { trpc } from "../../../utils/trpc";
+import { ProductForm } from "../../../components/ProductForm";
+
 const EditProductPage: NextPage = () => {
   const router = useRouter();
+
+  const productToModify = trpc.product.getProductToEdit.useQuery({
+    productId: `${router.query.id}`,
+  });
+
   return (
     <>
       <h1>Modificar Producto</h1>
+      <ProductForm action="edit" {...productToModify.data} />
     </>
   );
 };
