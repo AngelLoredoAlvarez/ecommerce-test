c2598ef4f6a6f769b07274174fc02f959685dfab
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-04 11:46:10 -0600

    Added the table that will show all the records in the Cart Model

--- src/pages/carrito.tsx
+++ src/pages/carrito.tsx
@@ -1,7 +1,73 @@
 import type { NextPage } from "next";
 
 const ShoppingCartPage: NextPage = () => {
-  return <div>Carrito Page</div>;
+  return (
+    <>
+      <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[3rem]">
+        Los Productos en el Carrito
+      </h1>
+
+      <div className="flex flex-col">
+        <div className="overflow-x-auto">
+          <div className="inline-block w-full p-1.5 align-middle">
+            <div className="overflow-hidden rounded-lg border">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
+                    >
+                      Código
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
+                    >
+                      Nombre
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
+                    >
+                      Descripción
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
+                    >
+                      Precio
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
+                    >
+                      Cantidad
+                    </th>
+                    <th
+                      className="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500 hover:text-green-400"
+                      colSpan={2}
+                      scope="col"
+                    />
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                  <tr>
+                    <td
+                      className="text-black-500 text-center text-5xl"
+                      colSpan={6}
+                    >
+                      No hay Productos en el Carrito{" "}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShoppingCartPage;
