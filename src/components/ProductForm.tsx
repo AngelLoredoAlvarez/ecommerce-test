fa0f90083e17aa162d7d6592f93a815aa0166659
Jesús Ángel Loredo Álvarez <angel.loredo.alvarez@gmail.com>
2022-11-03 22:21:38 -0600

    Added some conditional styling according the type of action that the user select

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -184,11 +184,15 @@ const ProductForm: FC<ProductFormProps> = (props) => {
         <div>
           <button
             type="submit"
-            className="group relative flex w-full justify-center
-                rounded-md border border-transparent bg-indigo-600 py-2 px-4
-                text-sm font-medium text-white hover:bg-indigo-700
+            className={`group relative flex w-full justify-center
+                rounded-md border border-transparent ${
+                  props.action === "add"
+                    ? "bg-green-500 hover:bg-green-600"
+                    : "bg-amber-500 hover:bg-amber-600"
+                } py-2 px-4
+                text-sm font-medium text-white
                 focus:outline-none focus:ring-2 focus:ring-indigo-500
-                focus:ring-offset-2"
+                focus:ring-offset-2`}
           >
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               {props.action === "add" ? (
